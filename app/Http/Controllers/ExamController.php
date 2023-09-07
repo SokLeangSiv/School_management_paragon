8d638f9823a2db5bedfef6220d7f3ebd99c73953
SokLeangSiv <lsok@paragoniu.edu.kh>
2023-09-07 23:14:26 +0700

    sixth commit

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -1,118 +1,135 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Exam;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use App\Models\stu_class;
 use Carbon\Carbon;
+use App\Models\stu_class;
+use App\Models\Teacher;
+use Illuminate\Support\Facades\DB;
 
-class ClassController extends Controller
+class ExamController extends Controller
 {
-    // Display a paginated list of classes based on search criteria
+
+    // Display the list of exams with search functionality
     public function index(Request $request){
-        // Get the search query from the request
+
+        // Search for exams
         $search = $request->search;
 
-        // Query the database to retrieve classes based on the search query
-        $class = stu_class::when($search, function ($query) use ($search){
-            $query->where('class_name','like','%' . $search . '%')
-                  ->orWhere('class_code', 'like', '%' . $search . '%')
-                  ->orWhere('teacher', 'like', '%' . $search . '%');
-        })->paginate(5);
+        $examinate = DB::table('exams')
+                    ->join('stu_classes', 'exams.class_id', '=', 'stu_classes.id')
+                    ->join('teachers', 'exams.teacher_id', '=', 'teachers.id')
+                    ->select('exams.*', 'stu_classes.class_name','stu_classes.class_code','teachers.teacher')
+                    ->when($search, function($query) use ($search){
 
-        // Return the view with the retrieved classes and search query
-        return view('admin.class.GetClass',compact('class','search'));
+                        $query->where('stu_classes.class_name', 'like', '%' . $search . '%')
+                        ->orWhere('teachers.teacher', 'like', '%' . $search . '%');
+
+                    })
+                    ->paginate(5);
+
+        return view('admin.exam.get_exam', compact('examinate', 'search'));
     }
 
-    // Display the form for adding a new class
-    public function AddClass(){
-        // Return the view for adding a new class
-        return view('admin.class.Add_Class');
+    // Display the page to add a new exam
+    public function AddExam(){
+
+        $teacher = Teacher::latest()->get();
+        $class = stu_class::latest()->get();
+        $exam = Exam::latest()->get();
+        return view('admin.exam.add_exam', compact('exam', 'class', 'teacher'));
+
     }
 
-    // Store a new class in the database
-    public function StoreClass(Request $request){
-        // Validate the incoming request data
+    // Store a new exam
+    public function StoreExam(Request $request)
+    {
+
         $request->validate([
-            'class_name' => 'required',
-            'class_code' => 'required|unique:stu_classes,class_code',
-            'teacher' => 'required',
-            'start_at' => 'required',
-            'end_at' => 'required',
+            'class_id' => 'required',
+            'teacher_id' => 'required',
             'day' => 'required',
-            'type' => 'required',
-            'status' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+            'group' => 'required',
+            'exam_type' => 'required',
+            'room' => 'required',
         ]);
 
-        // Create a new class instance and populate it with the request data
-        $class = new stu_class();
-        $class->class_name = Str::ucfirst($request->class_name);
-        $class->class_code =  Str::upper($request->class_code);
-        $class->teacher =  Str::ucfirst($request->teacher);
-        $class->start_at = $request->start_at;
-        $class->end_at =  $request->end_at;
-        $class->day =  $request->day;
-        $class->type =  $request->type;
-        $class->color =  rand(1, 5);
-        $class->status = $request->status;
-        $class->save();
+        $class = $request->class_id;
 
-        // Set a success notification and redirect to the class list
+        $exam = new Exam();
+        $exam->class_id = $class;
+        $exam->teacher_id = $request->teacher_id;
+        $exam->day = $request->day;
+        $exam->date = $request->date;
+        $exam->time = $request->time;
+        $exam->group = $request->group;
+        $exam->exam_type = $request->exam_type;
+        $exam->room = $request->room;
+
+        $exam->created_at = Carbon::now();
+
+        $exam->save();
+
         $notification = array(
-            'message' => 'Class Added Successfully',
+            'message' => 'Exam Inserted Successfully',
             'alert-type' => 'success'
         );
 
-        return redirect()->route('get.class')->with($notification);
+        return redirect()->route('get.exam')->with($notification);
+
     }
 
-    // Display the form for editing a class
-    public function EditClass($id){
-        // Find and retrieve the class to be edited
-        $class = stu_class::find($id);
+    // Display the page to edit an exam
+    public function EditExam($id){
 
-        // Return the view for editing the class
-        return view('admin.class.Edit_Class',compact('class'));
+        $exam = Exam::find($id);
+        $teacher = Teacher::latest()->get();
+        $class = stu_class::latest()->get();
+
+        return view('admin.exam.edit_exam', compact('exam', 'class', 'teacher'));
     }
 
-    // Update an existing class in the database
-    public function UpdateClass(Request $request, $id){
-        // Find and retrieve the class to be updated
-        $class = stu_class::find($id); 
+    // Update an existing exam
+    public function UpdateExam(Request $request, $id){
 
-        // Update the class properties with the incoming request data
-        $class->class_name = Str::ucfirst($request->class_name);
-        $class->class_code =  Str::upper($request->class_code);
-        $class->teacher =  Str::ucfirst($request->teacher);
-        $class->start_at = $request->start_at;
-        $class->end_at =  $request->end_at;
-        $class->day =  $request->day;
-        $class->type =  $request->type;
-        $class->status = $request->status;
-        $class->updated_at =Carbon::now();
-        $class->save();
+        $exam = Exam::find($id);
+        $exam->class_id = $request->class_id;
+        $exam->teacher_id = $request->teacher_id;
+        $exam->day = $request->day;
+        $exam->date = $request->date;
+        $exam->time = $request->time;
+        $exam->group = $request->group;
+        $exam->exam_type = $request->exam_type;
+        $exam->room = $request->room;
 
-        // Set a success notification and redirect to the class list
+        $exam->created_at = Carbon::now();
+
+        $exam->save();
+
         $notification = array(
-            'message' => 'Class Updated Successfully',
+            'message' => 'Exam Updated Successfully',
             'alert-type' => 'success'
         );
 
-        return redirect()->route('get.class')->with($notification);
+        return redirect()->route('get.exam')->with($notification);
+
     }
 
-    // Delete a class from the database
-    public function DeleteClass($id){
-        // Find and delete the class by its ID
-        $class = stu_class::find($id);
-        $class->delete();
-        // Set a success notification and redirect to the class list
+    // Delete an exam
+    public function DeleteExam($id){
+
+        $exam = Exam::find($id);
+        $exam->delete();
+
         $notification = array(
-            'message' => 'Class Deleted Successfully',
+            'message' => 'Exam Deleted Successfully',
             'alert-type' => 'success'
         );
 
-        return redirect()->route('get.class')->with($notification);
+        return redirect()->route('get.exam')->with($notification);
     }
+
 }
